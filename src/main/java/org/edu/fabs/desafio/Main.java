e51c642697d6e0a4a1bc75cd8c7b9fbae650a430
bianavic <bianavic@gmail.com>
2023-10-26 16:22:47 -0300

    finalize bootcamp challenge

--- src/main/java/org/edu/fabs/desafio/Main.java
+++ src/main/java/org/edu/fabs/desafio/Main.java
@@ -1,6 +1,8 @@
 package org.edu.fabs.desafio;
 
+import org.edu.fabs.desafio.domain.Bootcamp;
 import org.edu.fabs.desafio.domain.Course;
+import org.edu.fabs.desafio.domain.Dev;
 import org.edu.fabs.desafio.domain.Mentorship;
 
 import java.time.LocalDate;
@@ -21,25 +23,57 @@ public class Main {
 
         Course course1 = new Course();
         course1.setTitle("java");
-        course1.setDescription("descricao do curso java");
+        course1.setDescription("java course description");
         course1.setHour(8);
 
         Course course2 = new Course();
         course2.setTitle("javaScript");
-        course2.setDescription("descricao do curso javaScript");
+        course2.setDescription("javaScript course description");
         course2.setHour(8);
 
         Mentorship mentorship1 = new Mentorship();
-        mentorship1.setTitle("mentoria de java");
-        mentorship1.setDescription("descricao da mentoria java");
+        mentorship1.setTitle("java mentorship");
+        mentorship1.setDescription("java mentorship description");
         mentorship1.setMentorshipDate(LocalDate.now());
 
         Mentorship mentorship2 = new Mentorship();
-        mentorship2.setTitle("mentoria de javaScript");
-        mentorship2.setDescription("descricao da mentoria javaScript");
+        mentorship2.setTitle("javaScript mentorship");
+        mentorship2.setDescription("javaScript mentorship description");
         mentorship2.setMentorshipDate(LocalDate.now());
 
-        System.out.println(mentorship2);
+        // CRIAR bootcamps
+        Bootcamp bootcamp1 = new Bootcamp();
+        bootcamp1.setName("bootcamp java developer");
+        bootcamp1.setDescription("bootcamp java developer description");
+        bootcamp1.getContents().add(course1);
+        bootcamp1.getContents().add(course2);
+        bootcamp1.getContents().add(mentorship1);
+
+        // INSTACIAR devs
+        Dev dev1 = new Dev();
+        dev1.setName("Gonzales");
+        dev1.applyBootcamp(bootcamp1);
+        System.out.println("Gonzales Content applied: " + dev1.getContentApplied());
+        dev1.progression();
+        dev1.progression();
+        System.out.println(" - ");
+        System.out.println("Gonzales Content applied: " + dev1.getContentApplied());
+        System.out.println("Gonzales Content finalized: " + dev1.getContentFinalized());
+        System.out.println("XP: " + dev1.totalXPCalculator());
+
+        System.out.println("-------------------------");
+
+        Dev dev2 = new Dev();
+        dev2.setName("Nero");
+        dev2.applyBootcamp(bootcamp1);
+        System.out.println("Nero Content applied: " + dev2.getContentApplied());
+        dev2.progression();
+        dev2.progression();
+        dev2.progression(); // resultado: [] pq dev2 concluiu todos os cursos
+        System.out.println(" - ");
+        System.out.println("Nero Content applied: " + dev2.getContentApplied());
+        System.out.println("Nero Content finalized: " + dev2.getContentFinalized());
+        System.out.println("XP: " + dev2.totalXPCalculator());
 
     }
 
